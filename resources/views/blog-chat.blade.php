b9b5b6c26b188908865f1525000fa458a3cea00a
Rushad Tanjim <rushadtanjim3@gmail.com>
2023-08-19 00:27:12 +0600

    Added:Chating in frontend

--- resources/views/blog-chat.blade.php
+++ resources/views/blog-chat.blade.php
@@ -1,3 +1,13 @@
 <x-layout>
-    <p> Name: <span id="chat"></span></p>
+    <div class="col-md-12 col-sm-12">
+        <div class="col" id="chat">
+        </div>
+        <div class="container">
+            <form action="/chat" method="POST" class="form-group">
+                @csrf
+                <input type="text" class="form-control" name="chatmessage" placeholder="Type your message">
+                <button type="submit" class="btn btn-primary"><i class="fas fa-arrow-right"></i></button>        
+            </form>
+        </div>
+    </div>
 </x-layout>
